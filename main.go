a77e04122c266ab8e064db854e851b7b30563244
Matt Mulhern <mattmulhern01@gmail.com>
2020-02-02 15:56:58 +0000

    default to running client
    Signed-off-by: Matt Mulhern <mattmulhern01@gmail.com>

--- main.go
+++ main.go
@@ -12,7 +12,8 @@ import (
 func main() {
 	argsWithoutProg := os.Args[1:]
 	if len(argsWithoutProg) == 0 {
-		os.Exit(1)
+		fmt.Printf("running client...\n")
+		client.Run()
 	}
 	switch argsWithoutProg[0] {
 	case "server":
@@ -25,7 +26,7 @@ func main() {
 		fmt.Println("dog")
 		dog.Run()
 	default:
-		fmt.Printf("bad opt! [%s]\n", argsWithoutProg[0])
-		os.Exit(1)
+		fmt.Printf("bad opt! [%s]\n defaulting to client\n", argsWithoutProg[0])
+		client.Run()
 	}
 }
